464e2ef88b564c6121c711ae8de449cfdc4d970d
Victor Gama <hey@vito.io>
2017-03-14 20:30:16 -0300

    Avoid overflows by using a bigger type

--- BBRealTimeCurrencyFormatter/BBRealTimeCurrencyFormatter.m
+++ BBRealTimeCurrencyFormatter/BBRealTimeCurrencyFormatter.m
@@ -17,12 +17,9 @@
     //get current text after typing
     NSString * currentText = [textField.text stringByReplacingCharactersInRange:range withString:string];
     
-    //get current value from string
-    NSDecimalNumber *currentDecimal = [NSDecimalNumber decimalNumberWithString:[self keepOnlyNumbers:currentText]];
-    
     //format amount into a string
-    NSString *newValueString = [self amountToString:currentDecimal includeCurrencySymbol:YES];
-    
+    NSString *newValueString = [self amountToString:[[self keepOnlyNumbers:currentText] doubleValue] includeCurrencySymbol:YES];
+
     //update textfield
     [textField setText:newValueString];
     
@@ -32,28 +29,35 @@
 
 //Remove all characters BUT numbers
 + (nullable NSString *)keepOnlyNumbers:(nonnull NSString *)string{
-    return [[string componentsSeparatedByCharactersInSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]] componentsJoinedByString:@""];
+    static dispatch_once_t onceToken;
+    static NSRegularExpression *exp;
+    dispatch_once(&onceToken, ^{
+        exp = [NSRegularExpression regularExpressionWithPattern:@"[^\\d]" options:0 error:nil];
+    });
+    return [exp stringByReplacingMatchesInString:string options:0 range:NSMakeRange(0, string.length) withTemplate:@""];
 }
 
 //Return a currency formatted string
-+ (nonnull NSString *)amountToString:(nonnull NSDecimalNumber *)amount includeCurrencySymbol:(BOOL)symbol{
++ (nonnull NSString *)amountToString:(double)amount includeCurrencySymbol:(BOOL)symbol{
     
     NSLocale *locale  = [NSLocale currentLocale];
     
     //Convert it to currency
     NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
     [numberFormatter setLocale:locale];
-    [numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle]; //Set currency style
-    [numberFormatter setMultiplier:[NSNumber numberWithFloat:.01]];  //Ensure cents calculation
+
+    //Set currency style
+    [numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
     [numberFormatter setDecimalSeparator:[locale objectForKey:NSLocaleDecimalSeparator]];
     [numberFormatter setCurrencyGroupingSeparator:[locale objectForKey:NSLocaleGroupingSeparator]];
     
     if(!symbol){
-        [numberFormatter setCurrencySymbol:@""]; //Remove currency symbol - This'll avoid NaN results
+        //Remove currency symbol - This'll avoid NaN results
+        [numberFormatter setCurrencySymbol:@""];
     }
     
     //return new amount string
-    return [numberFormatter stringFromNumber:amount];
+    return [numberFormatter stringFromNumber:@(amount / 100.0f)];
 }
 
 @end
